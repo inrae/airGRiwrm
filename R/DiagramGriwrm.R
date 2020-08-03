bc10e4ee20090c638469737ae39a00b3c3f16afb
Dorchies David <david.dorchies@inrae.fr>
2020-08-03 17:24:19 +0200

    feat: Add network diagrams
    - Add mermaid diagrams in vignette V04
    - Add function `DiagramGriwrm` for generating automatic diagrams
    
    Refs #5

---
+++ R/DiagramGriwrm.R
@@ -0,0 +1,33 @@
+#' Display a diagram representing the network structure of a Griwrm object
+#'
+#' @param griwrm the Griwrm object to display.
+#' @param display if `TRUE` displays the diagram with `DiagrammeR::mermaid`, return the mermaid code otherwise.
+#' @param orientation Orientation of the graph. "LR" by default.
+#'
+#' @return Mermaid code of the diagram id display is `FALSE`.
+#' @export
+#'
+#' @examples
+#' # Display diagram
+#' DiagramGriwrm(griwrm)
+#' # Is the same as
+#' DiagrammeR::mermaid(DiagramGriwrm(griwrm, display = FALSE), width = "100%", height = "100%")
+#'
+DiagramGriwrm <- function(griwrm, display = TRUE, orientation = "LR") {
+  g2 <- griwrm[!is.na(griwrm$down),]
+  nodes <- paste(
+    g2$id,
+    "-->|",
+    format(g2$length/1000, trim = TRUE, digits = 0),
+    "km|",
+    g2$down
+  )
+  styleSD <- paste("style", unique(g2$down), "fill:#cfc")
+  styleDF <- paste("style", unique(g2$id[is.na(g2$model)]), "fill:#fcc")
+  diagram <- paste(c(paste("graph", orientation), nodes, styleSD, styleDF), collapse = "\n")
+  if(display) {
+    DiagrammeR::mermaid(diagram, width = "100%", height = "100%")
+  } else {
+    return(diagram)
+  }
+}
