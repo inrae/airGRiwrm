9b1ce11d1778703649e29906cb915b2bd996a701
Dorchies David <david.dorchies@irstea.fr>
2020-12-23 11:09:09 +0100

    feat: convett basin meteo to sub-basin level
    Fix #21

---
+++ R/ConvertMeteoSD.R
@@ -0,0 +1,65 @@
+#' Convert meteorological data from basin scale to sub-basin scale
+#'
+#' @param x either a [GRiwrm] network description, a [character] id of a node, a [matrix] containing meteorological data
+#' @param ... Parameters passed to the methods
+#'
+#' @return Either a [matrix] containing the converted meteorological data
+#' @export
+#' @rdname ConvertMeteoSD
+#'
+#' @examples
+ConvertMeteoSD <- function(x, ...) {
+  UseMethod("ConvertMeteoSD")
+}
+
+#' @export
+#' @rdname ConvertMeteoSD
+ConvertMeteoSD.GRiwrm <- function(x, meteo, ...) {
+  meteo <- as.matrix(meteo)
+  output <- lapply(colnames(meteo), ConvertMeteoSD , griwrm = x, meteo = meteo)
+  meteoOut <- do.call(cbind,output)
+  dimnames(meteoOut)[[2]] <- colnames(meteo)
+  return(meteoOut)
+}
+
+#' @export
+#' @rdname ConvertMeteoSD
+ConvertMeteoSD.character <- function(x, griwrm, meteo) {
+  griwrmUp <- griwrm[!is.na(griwrm$down),]
+  cols <- c(x, griwrmUp$id[griwrmUp$down == x])
+  output <- ConvertMeteoSD(meteo[,cols, drop = FALSE], griwrm$area[match(cols, griwrm$id)])
+  return(output)
+}
+
+#' @export
+#' @rdname ConvertMeteoSD
+ConvertMeteoSD.matrix <- function(x, areas, temperature = FALSE) {
+  # Check arguments
+  if(nrow(x) < 2) {
+    stop("Meteorological data matrix should contain more than one row")
+  }
+  if(length(areas) != ncol(x)) {
+    stop("'areas' length and meteo data matrix number of columns should be equal")
+  }
+  if(areas[1] <= sum(areas[-1])) {
+    stop("Basin area 'areas[1]' should be greater than the sum of the upstream sub-basin areas")
+  }
+  if(ncol(x) == 1) {
+    return(x)
+  }
+  # Convert mm to 1E3 m3
+  V <- x * rep(areas, rep(nrow(x), length(areas)))
+  # Sum upstream data
+  if(ncol(x) > 2) {
+    Vup <- rowSums(V[,-1])
+  } else {
+    Vup <- V[,2]
+  }
+  # Remove to basin to get downstream data
+  Vdown <- V[,1] - Vup
+  if(!temperature) Vdown[Vdown < 0] <- 0
+  # Convert to mm
+  meteoDown <- Vdown / (areas[1] - sum(areas[-1]))
+  return(as.matrix(meteoDown, ncol = 1))
+}
+
