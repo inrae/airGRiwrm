568d3f589578c04b52dfc534ed9287f09beca7b7
Dorchies David <david.dorchies@irstea.fr>
2020-12-28 18:27:08 +0100

    feat: error message for 'DiagrammeR' package suggested dependency
    Fix #24

--- R/DiagramGRiwrm.R
+++ R/DiagramGRiwrm.R
@@ -23,6 +23,9 @@ DiagramGRiwrm <- function(griwrm, display = TRUE, orientation = "LR") {
   if(Sys.getenv("RSTUDIO") != "1") {
     return()
   }
+  if(!"DiagrammeR" %in% rownames(installed.packages())) {
+    stop("The 'DiagrammeR' package should be installed. Type: install.packages('DiagrammeR')")
+  }
   g2 <- griwrm[!is.na(griwrm$down),]
   nodes <- paste(
     g2$id,
