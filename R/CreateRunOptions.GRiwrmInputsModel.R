5d411323546f7406d88525229642b671704b99be
Dorchies David <david.dorchies@inrae.fr>
2021-07-12 16:04:57 +0200

    feat(RunOptions): add IniStates argument for a list of initial states
    Refs #48

--- R/CreateRunOptions.GRiwrmInputsModel.R
+++ R/CreateRunOptions.GRiwrmInputsModel.R
@@ -1,12 +1,13 @@
+#' @param IniStates (optional) [numeric] object or [list] of [numeric] object of class \emph{IniStates}, see [airGR::CreateIniStates] for details
 #' @rdname CreateRunOptions
 #' @export
-CreateRunOptions.GRiwrmInputsModel <- function(InputsModel, ...) {
+CreateRunOptions.GRiwrmInputsModel <- function(InputsModel, IniStates = NULL, ...) {
 
   RunOptions <- list()
   class(RunOptions) <- append(class(RunOptions), "GRiwrmRunOptions")
 
-  for(InputsModelBasin in InputsModel) {
-    RunOptions[[InputsModelBasin$id]] <- CreateRunOptions(InputsModel = InputsModelBasin, ...)
+  for(id in names(InputsModel)) {
+    RunOptions[[id]] <- CreateRunOptions(InputsModel = InputsModel[[id]], IniStates = IniStates[[id]], ...)
   }
   return(RunOptions)
 }
--- R/CreateRunOptions.R
+++ R/CreateRunOptions.R
@@ -7,9 +7,11 @@
 #'
 #' @details See [airGR::CreateRunOptions] documentation for a complete list of arguments.
 #'
+#' If `InputsModel` argument is a \emph{GRiwrmInputsModel} object, `IniStates` must be a list of [numeric] object of class \emph{IniStates} with one item per modelled sub-catchment.
+#'
 #' With a \emph{GRiwrmInputsModel} object, all arguments are applied on each sub-catchments of the network.
 #'
-#' @return Depending on the class of `InputsModel` argument (respectively `InputsModel` and `GRiwrmInputsModel` object), the returned value is respectively:
+#' @return Depending on the class of `InputsModel` argument (respectively \emph{InputsModel} and \emph{GRiwrmInputsModel} object), the returned value is respectively:
 #' - a `RunOptions` object (See [airGR::CreateRunOptions])
 #' - a `GRiwrmRunOptions` object which is a [list] of `RunOptions` object with one item per modelled sub-catchment
 #'
