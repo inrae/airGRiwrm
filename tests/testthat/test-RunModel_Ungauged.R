ab310938c4ae29dd04d8771ef8090cdf6a9ee38e
David <david.dorchies@inrae.fr>
2023-02-22 15:41:34 +0100

    test: Add crash test on ungauged node with diversion
    Refs #114

--- tests/testthat/test-RunModel_Ungauged.R
+++ tests/testthat/test-RunModel_Ungauged.R
@@ -103,3 +103,43 @@ test_that("RunModel_Ungauged works with a diversion as upstream node (#113)", {
   OCdiv <- Calibration(InputsModel, RunOptions, IC, CO)
   expect_equal(OCdiv$`54032`$CritFinal, CritValue)
 })
+
+test_that("Ungauged node with diversion outside the sub-network shoudl work", {
+  nodes <- loadSevernNodes()
+  nodes <- nodes[!nodes$id %in% c("54002", "54057", "54029"), ]
+  nodes[nodes$id == "54032", c("down", "length")] <- c(NA, NA)
+  nodes$model[nodes$id == "54095"] <- "Ungauged"
+  nodes <- rbind(nodes,
+                 data.frame(id = "54095", down = "54032", length = 100,
+                            area = NA, model = "Diversion"))
+
+  g <- CreateGRiwrm(nodes)
+  Qobs2 <- matrix(0, ncol = 1, nrow = 11536)
+  colnames(Qobs2) <- "54095"
+  e <- setupRunModel(griwrm = g, runRunModel = FALSE, Qobs2 = Qobs2)
+  for(x in ls(e)) assign(x, get(x, e))
+  np <- getAllNodesProperties(griwrm)
+
+  IC <- CreateInputsCrit(
+    InputsModel,
+    FUN_CRIT = ErrorCrit_KGE2,
+    RunOptions = RunOptions,
+    Obs = Qobs[IndPeriod_Run, np$id[np$RunOff & np$calibration == "Gauged"], drop = FALSE],
+    transfo = "sqrt",
+    k = 0.15
+  )
+
+  CO <- CreateCalibOptions(InputsModel)
+  OC <- Calibration(InputsModel, RunOptions, IC, CO)
+  Param <- sapply(OC, "[[", "ParamFinalR")
+  OM <- RunModel(
+    InputsModel,
+    RunOptions = RunOptions,
+    Param = Param
+  )
+  CritValue <- ErrorCrit_KGE2(
+    InputsCrit = IC$`54032`,
+    OutputsModel = OM$`54032`
+  )$CritValue
+
+})
