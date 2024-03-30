294bc1b098b63e9c72c04a90bd5df4b45b943a6f
David <david.dorchies@inrae.fr>
2024-03-30 15:02:39 +0100

    fix(check): notes on undefined variables
    Refs #130

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+#' getNodeRanking: no visible binding for global variable 'donor'
+#' updateParameters4Ungauged: no visible binding for global variable
+#' down'
+#' updateParameters4Ungauged: no visible binding for global variable
+#' model'
+utils::globalVariables(c("donor", "down ", "model "))
