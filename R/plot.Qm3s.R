inrae/airGRiwrm

commit cb6472c7abf0e9377337074256774a4b1ff13c30

change(plot.Qm3s): change to nicer palette

R/plot.Qm3s.R
@@ -22,7 +22,6 @@
 #'
 #' @example man-examples/RunModel.GRiwrmInputsModel.R
 #'
-#' @importFrom grDevices rainbow
 #' @importFrom graphics matplot
 #' @export plot.Qm3s
 #' @export
@@ -32,7 +31,7 @@ plot.Qm3s <- function(x,
                       xlab = "Date",
                       ylab = expression("Flow rate (m"^"3"*"/s)"),
                       main = "Simulated flows",
-                      col = rainbow(ncol(x) - 1),
+                      col = grDevices::hcl.colors(ncol(x) - 1, "Spectral"),
                       legend = colnames(x)[-1],
                       legend.cex = 0.7,
                       legend.x = "topright",

man/plot.Qm3s.Rd
@@ -10,7 +10,7 @@
   xlab = "Date",
   ylab = expression("Flow rate (m"^"3" * "/s)"),
   main = "Simulated flows",
-  col = rainbow(ncol(x) - 1),
+  col = grDevices::hcl.colors(ncol(x) - 1, "Spectral"),
   legend = colnames(x)[-1],
   legend.cex = 0.7,
   legend.x = "topright",

NAMESPACE
@@ -51,7 +51,6 @@ export(plot.Qm3s)
 import(airGR)
 import(dplyr)
 importFrom(dplyr,"%>%")
-importFrom(grDevices,rainbow)
 importFrom(graphics,matplot)
 importFrom(graphics,par)
 importFrom(graphics,plot)

commit 9045b3dc746ea459d29d8bd59c2164d853c50e00

fix(plot.Qm3s): crash with one column
Regression after cb6472c7abf0e9377337074256774a4b1ff13c30 and 94b7ab80386504d4ca277b4a3bd5a0774b59bafb

R/plot.Qm3s.R
@@ -10,7 +10,7 @@
 #' @param xlab [character] label for the x axis, default to "Date"
 #' @param ylab [character] label for the y axis, default to "Flow (m3/s)"
 #' @param main [character] main title for the plot, default to "Simulated flows"
-#' @param col [character] plotting color (See [par]), default to rainbow colors
+#' @param col [character] plotting colors (See [par])
 #' @param legend [character] see parameter `legend` of [legend]. Set it to [NULL]
 #'        to hide the legend
 #' @param legend.cex [character] `cex` parameter for the text of the legend (See [par])
@@ -22,7 +22,6 @@
 #'
 #' @example man-examples/RunModel.GRiwrmInputsModel.R
 #'
-#' @importFrom graphics matplot
 #' @export plot.Qm3s
 #' @export
 #'
@@ -42,7 +41,12 @@ plot.Qm3s <- function(x,
   stopifnot(is.data.frame(x),
             inherits(x[, 1], "POSIXct"))
 
-  matplot(
+  col <- tryCatch(
+    length(col),
+    error = function(e) "#619CFF"
+  )
+
+  graphics::matplot(
     x[, 1],
     x[, -1],
     type = type,

man/plot.Qm3s.Rd
@@ -31,7 +31,7 @@ columns with flows at each node of the network}
 
 \item{main}{\link{character} main title for the plot, default to "Simulated flows"}
 
-\item{col}{\link{character} plotting color (See \link{par}), default to rainbow colors}
+\item{col}{\link{character} plotting colors (See \link{par})}
 
 \item{legend}{\link{character} see parameter \code{legend} of \link{legend}. Set it to \link{NULL}
 to hide the legend}
@@ -250,17 +250,18 @@ Ind_Plot <- which(
   OM_div[[1]]$DatesR >= as.POSIXct("2003-01-01", tz = "UTC") &
   OM_div[[1]]$DatesR <= as.POSIXct("2003-12-31", tz = "UTC")
 )
-dfQdiv <- data.frame(DatesR = OM_div[[1]]$DatesR[Ind_Plot],
+dfQdiv <- as.Qm3s(DatesR = OM_div[[1]]$DatesR[Ind_Plot],
                      Diverted_flow = Qdiv_m3s[Ind_Plot])
 
 oldpar <- par(mfrow=c(2,1), mar = c(2.5,4,1,1))
-plot.Qm3s(dfQdiv)
+plot(dfQdiv)
 
 # Plot natural and influenced flow at station "54001"
 df54001 <- cbind(attr(OM_div, "Qm3s")[Ind_Plot, c("DatesR", "54001")],
                  attr(OM_severn, "Qm3s")[Ind_Plot, "54001"])
 names(df54001) <- c("DatesR", "54001 with irrigation", "54001 natural flow")
-plot.Qm3s(df54001, ylim = c(0,70))
+df54001 <- as.Qm3s(df54001)
+plot(df54001, ylim = c(0,70))
 abline(h = 12, col = "green")
 par(oldpar)
 }